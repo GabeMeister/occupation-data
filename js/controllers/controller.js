21ff74596bee89013d73f5cb547dfe130043e346
Gabe Jensen <gabe.jensen@wsu.edu>
2017-02-18 09:39:26 -0800

    showing all raw data in view

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,26 +1,50 @@
-app.controller("OccupationController", function($scope, $http) {
-    
-    
-    $scope.fetchData = function() {
-        $http.get("data/15-1131.json").then(
-            function(response) {
-                var data = response.data;
-                $scope.occupation = data.occupation;
-                $scope.region = data.region;
-                $scope.summary = data.summary;
-                $scope.trendComparison = data.trend_comparison;
-                $scope.employingIndustries = data.employing_industries;
+app.controller("OccupationController", function ($scope, $http) {
 
-                $scope.dataLoaded = true;
-            }, 
-            function(response) {
-                console.error("Unable to correctly fetch data");
-                console.error(response);
-            });
+    $scope.aboveOrBelow = function(num1, num2) {
+        text = "above";
+        if (num1 < num2) {
+            text = "below";
+        }
+
+        return text;
+    }
+
+    $scope.plusOrMinus = function(num1) {
+        var symbol = "+";
+        if (num1 < 0) {
+            symbol = "-";
+        }
+
+        return symbol;
+    }
+
+    $scope.fetchData = function () {
+        $scope.dataLoaded = false;
+        var dataFileName = "data/" + $scope.occupationID + "_" + $scope.areaType + "_" + $scope.areaCode + ".json";
+        $http.get(dataFileName).then(handleData, handleError);
+    }
+
+    function handleData(response) {
+        var data = response.data;
+        $scope.occupation = data.occupation;
+        $scope.region = data.region;
+        $scope.summary = data.summary;
+        $scope.trendComparison = data.trend_comparison;
+        $scope.employingIndustries = data.employing_industries;
+
+        $scope.dataLoaded = true;
+    }
+
+    function handleError(response) {
+        console.error("Unable to correctly fetch data");
+        console.error(response);
     }
 
     function init() {
         $scope.dataLoaded = false;
+        $scope.occupationID = "15-1131";
+        $scope.areaType = "msa";
+        $scope.areaCode = "42660";
 
         // TODO: remove this
         $scope.fetchData();
